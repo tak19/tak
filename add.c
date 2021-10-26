8b12e127e2fa867408526dddc0447ee692841da1
ghksdl1224 <ghksdl1224@gmail.com>
2021-10-25 17:41:20 -0700

    first

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int add(int x, int y){
+	
+	printf("%d + %d = %d\n", x, y, x+y);
+	
+	return 0;
+	
+	}
+	
+
+
---
+++ divide.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int divide(int x, int y){
+	
+	printf("%d / %d = %d\n", x, y, x/y);
+	
+	return 0;
+	}
+	
