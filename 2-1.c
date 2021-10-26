977acc2ca9cbc5fd3aee4eb8418044978daf0856
tak19 <tso5245@naver.com>
2021-10-25 17:47:22 -0700

    first

---
+++ 2-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lib/test.h"
+void main() {
+	printf("add(25,5) = %d\n",add(25,5));
+	printf("sub(25,5) = %d\n",sub(25,5));
+	printf("mult(25,5) = %d\n",mult(25,5));
+	printf("divi(25,5) = %d\n",divi(25,5));
+
+	}
